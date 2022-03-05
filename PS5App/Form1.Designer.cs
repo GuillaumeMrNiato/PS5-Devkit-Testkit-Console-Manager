5768c2bca480e4e870bdecf8df55deae1f1aa35f
Guillaume MrNiato <boost@MRNIATO>
2022-03-05 21:07:31 +0100

    fix "developement" word

--- PS5App/Form1.Designer.cs
+++ PS5App/Form1.Designer.cs
@@ -64,10 +64,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 47);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(17, 78);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(264, 22);
+            this.button1.Size = new System.Drawing.Size(377, 37);
             this.button1.TabIndex = 0;
             this.button1.Text = "Connect the PS5";
             this.button1.UseVisualStyleBackColor = true;
@@ -76,10 +75,9 @@
             // button2
             // 
             this.button2.Enabled = false;
-            this.button2.Location = new System.Drawing.Point(13, 51);
-            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Location = new System.Drawing.Point(19, 85);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(162, 22);
+            this.button2.Size = new System.Drawing.Size(231, 37);
             this.button2.TabIndex = 2;
             this.button2.Text = "Set Mode";
             this.button2.UseVisualStyleBackColor = true;
@@ -93,11 +91,9 @@
             this.groupBox1.Controls.Add(this.button5);
             this.groupBox1.Controls.Add(this.textBox1);
             this.groupBox1.Controls.Add(this.button1);
-            this.groupBox1.Location = new System.Drawing.Point(8, 7);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Location = new System.Drawing.Point(11, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.groupBox1.Size = new System.Drawing.Size(500, 104);
+            this.groupBox1.Size = new System.Drawing.Size(714, 173);
             this.groupBox1.TabIndex = 3;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Connection";
@@ -105,10 +101,9 @@
             // button3
             // 
             this.button3.Enabled = false;
-            this.button3.Location = new System.Drawing.Point(192, 74);
-            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button3.Location = new System.Drawing.Point(274, 123);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(85, 22);
+            this.button3.Size = new System.Drawing.Size(121, 37);
             this.button3.TabIndex = 8;
             this.button3.Text = "Power Off";
             this.button3.UseVisualStyleBackColor = true;
@@ -120,11 +115,9 @@
             this.groupBox3.Controls.Add(this.label3);
             this.groupBox3.Controls.Add(this.label2);
             this.groupBox3.Controls.Add(this.label1);
-            this.groupBox3.Location = new System.Drawing.Point(282, 20);
-            this.groupBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox3.Location = new System.Drawing.Point(403, 33);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.groupBox3.Size = new System.Drawing.Size(214, 76);
+            this.groupBox3.Size = new System.Drawing.Size(306, 127);
             this.groupBox3.TabIndex = 7;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Informations";
@@ -132,50 +125,45 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(56, 45);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(80, 75);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(88, 15);
+            this.label4.Size = new System.Drawing.Size(132, 25);
             this.label4.TabIndex = 3;
             this.label4.Text = "Not Connected";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(30, 19);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(43, 32);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(88, 15);
+            this.label3.Size = new System.Drawing.Size(132, 25);
             this.label3.TabIndex = 2;
             this.label3.Text = "Not Connected";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(4, 45);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(6, 75);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(48, 15);
+            this.label2.Size = new System.Drawing.Size(73, 25);
             this.label2.TabIndex = 1;
             this.label2.Text = "Owner :";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(4, 18);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(6, 30);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(23, 15);
+            this.label1.Size = new System.Drawing.Size(36, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "IP :";
             // 
             // button4
             // 
             this.button4.Enabled = false;
-            this.button4.Location = new System.Drawing.Point(103, 74);
-            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button4.Location = new System.Drawing.Point(147, 123);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(85, 22);
+            this.button4.Size = new System.Drawing.Size(121, 37);
             this.button4.TabIndex = 9;
             this.button4.Text = "Rest Mode";
             this.button4.UseVisualStyleBackColor = true;
@@ -184,10 +172,9 @@
             // button5
             // 
             this.button5.Enabled = false;
-            this.button5.Location = new System.Drawing.Point(13, 74);
-            this.button5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button5.Location = new System.Drawing.Point(19, 123);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(85, 22);
+            this.button5.Size = new System.Drawing.Size(121, 37);
             this.button5.TabIndex = 10;
             this.button5.Text = "Reboot";
             this.button5.UseVisualStyleBackColor = true;
@@ -195,10 +182,9 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(13, 20);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Location = new System.Drawing.Point(19, 33);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(265, 23);
+            this.textBox1.Size = new System.Drawing.Size(377, 31);
             this.textBox1.TabIndex = 1;
             this.textBox1.Text = "192.168.1.126";
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
@@ -211,21 +197,18 @@
             this.groupBox2.Controls.Add(this.textBox2);
             this.groupBox2.Controls.Add(this.comboBox1);
             this.groupBox2.Controls.Add(this.button2);
-            this.groupBox2.Location = new System.Drawing.Point(8, 115);
-            this.groupBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Location = new System.Drawing.Point(11, 192);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.groupBox2.Size = new System.Drawing.Size(500, 81);
+            this.groupBox2.Size = new System.Drawing.Size(714, 135);
             this.groupBox2.TabIndex = 6;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Other shit";
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(409, 34);
-            this.button6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button6.Location = new System.Drawing.Point(584, 57);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(78, 20);
+            this.button6.Size = new System.Drawing.Size(111, 33);
             this.button6.TabIndex = 7;
             this.button6.Text = "...";
             this.button6.UseVisualStyleBackColor = true;
@@ -234,29 +217,26 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(207, 19);
-            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Location = new System.Drawing.Point(296, 32);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(98, 15);
+            this.label6.Size = new System.Drawing.Size(150, 25);
             this.label6.TabIndex = 6;
             this.label6.Text = "File Serving root :";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(192, 13);
-            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Location = new System.Drawing.Point(274, 22);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(10, 60);
+            this.label5.Size = new System.Drawing.Size(16, 100);
             this.label5.TabIndex = 5;
             this.label5.Text = "|\r\n|\r\n|\r\n|\r\n";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(207, 36);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox2.Location = new System.Drawing.Point(296, 60);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(199, 23);
+            this.textBox2.Size = new System.Drawing.Size(283, 31);
             this.textBox2.TabIndex = 4;
             // 
             // comboBox1
@@ -265,11 +245,10 @@
             this.comboBox1.Items.AddRange(new object[] {
             "Release Mode",
             "Assist Mode",
-            "Developement Mode"});
-            this.comboBox1.Location = new System.Drawing.Point(13, 24);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            "Development Mode"});
+            this.comboBox1.Location = new System.Drawing.Point(19, 40);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(162, 23);
+            this.comboBox1.Size = new System.Drawing.Size(230, 33);
             this.comboBox1.TabIndex = 3;
             // 
             // statusStrip1
@@ -284,71 +263,69 @@
             this.toolStripStatusLabel6,
             this.toolStripStatusLabel7,
             this.toolStripStatusLabel8});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 210);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 355);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 10, 0);
-            this.statusStrip1.Size = new System.Drawing.Size(519, 22);
+            this.statusStrip1.Size = new System.Drawing.Size(741, 32);
             this.statusStrip1.TabIndex = 8;
             this.statusStrip1.Text = "statusStrip1";
             // 
             // toolStripStatusLabel1
             // 
             this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            this.toolStripStatusLabel1.Size = new System.Drawing.Size(56, 17);
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(85, 25);
             this.toolStripStatusLabel1.Text = "Console :";
             // 
             // toolStripStatusLabel2
             // 
             this.toolStripStatusLabel2.ForeColor = System.Drawing.Color.Red;
             this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
-            this.toolStripStatusLabel2.Size = new System.Drawing.Size(88, 17);
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(132, 25);
             this.toolStripStatusLabel2.Text = "Not Connected";
             // 
             // toolStripStatusLabel3
             // 
             this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
-            this.toolStripStatusLabel3.Size = new System.Drawing.Size(10, 17);
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(16, 25);
             this.toolStripStatusLabel3.Text = "|";
             // 
             // toolStripStatusLabel4
             // 
             this.toolStripStatusLabel4.Name = "toolStripStatusLabel4";
-            this.toolStripStatusLabel4.Size = new System.Drawing.Size(37, 17);
+            this.toolStripStatusLabel4.Size = new System.Drawing.Size(58, 25);
             this.toolStripStatusLabel4.Text = "Type :";
             // 
             // toolStripStatusLabel5
             // 
             this.toolStripStatusLabel5.Name = "toolStripStatusLabel5";
-            this.toolStripStatusLabel5.Size = new System.Drawing.Size(26, 17);
+            this.toolStripStatusLabel5.Size = new System.Drawing.Size(41, 25);
             this.toolStripStatusLabel5.Text = "Idle";
             // 
             // toolStripStatusLabel6
             // 
             this.toolStripStatusLabel6.Name = "toolStripStatusLabel6";
-            this.toolStripStatusLabel6.Size = new System.Drawing.Size(10, 17);
+            this.toolStripStatusLabel6.Size = new System.Drawing.Size(16, 25);
             this.toolStripStatusLabel6.Text = "|";
             // 
             // toolStripStatusLabel7
             // 
             this.toolStripStatusLabel7.Name = "toolStripStatusLabel7";
-            this.toolStripStatusLabel7.Size = new System.Drawing.Size(75, 17);
+            this.toolStripStatusLabel7.Size = new System.Drawing.Size(117, 25);
             this.toolStripStatusLabel7.Text = "SDK Version :";
             // 
             // toolStripStatusLabel8
             // 
             this.toolStripStatusLabel8.Name = "toolStripStatusLabel8";
-            this.toolStripStatusLabel8.Size = new System.Drawing.Size(26, 17);
+            this.toolStripStatusLabel8.Size = new System.Drawing.Size(41, 25);
             this.toolStripStatusLabel8.Text = "Idle";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(519, 232);
+            this.ClientSize = new System.Drawing.Size(741, 387);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
